6fac988c28d1e94b842d4d95d38de571739e5495
acousma <1215128451@qq.com>
2025-05-28 09:25:04 +0800

    新增员工代码ssh

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -24,13 +24,4 @@ public interface EmployeeMapper {
             +" values " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
-    /**
-     * 新增插入一条员工书
-     * @param employee
-     */
-    @Insert("insert into employee (name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user)"
-            +" values " +
-            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
-    void insert2(Employee employee);
-
 }
